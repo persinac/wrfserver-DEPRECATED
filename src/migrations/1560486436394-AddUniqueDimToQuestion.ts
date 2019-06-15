bba48e9ab4db3effda40e50a4f5b02ce305847d2
Alex Persinger <apfbacc@gmail.com>
2019-06-14 22:07:56 -0400

    Added extra queries in comments. Need to add more.

--- src/migrations/1560486436394-AddUniqueDimToQuestion.ts
+++ src/migrations/1560486436394-AddUniqueDimToQuestion.ts
@@ -14,3 +14,17 @@ export class AddUniqueDimToQuestion1560486436394 implements MigrationInterface {
     }
 
 }
+
+/***
+ OTHER QUERIES
+
+ INSERT INTO wrf.question (text, short_name, topic, tooltip, instructions, datatype, is_active, created_by, updated_by, cat_fk, `grouping`, unique_dim)
+ VALUES
+ ('Length', 'cab_lngth_3', 'length', 'enter a length', '', 'number', 1, 'admin', 'admin', 10, 4, 'cab_3'),
+ ('Width', 'cab_wdth_3', 'width', 'enter a width', '', 'number', 1, 'admin', 'admin', 10, 4, 'cab_3'),
+ ('Height', 'cab_height_3', 'height', 'enter a height', '', 'number', 1, 'admin', 'admin', 10, 4, 'cab_3'),
+ ('Length', 'cab_lngth_4', 'length', 'enter a length', '', 'number', 1, 'admin', 'admin', 10, 5, 'cab_4'),
+ ('Width', 'cab_wdth_4', 'width', 'enter a width', '', 'number', 1, 'admin', 'admin', 10, 5, 'cab_4'),
+ ('Height', 'cab_height_4', 'height', 'enter a height', '', 'number', 1, 'admin', 'admin', 10, 5, 'cab_4');
+ ****/
+ 
